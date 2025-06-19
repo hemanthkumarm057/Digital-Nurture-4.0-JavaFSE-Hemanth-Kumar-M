12594bc8a2a1ca802c20190373fc157485829546
hemanthkumarm057 <hemanthkumarm057@gmail.com>
2025-06-19 19:03:30 +0530

    Add files via upload

---
+++ Week-01/3.E-commerce_Platform_Search_Function/SearchUtility.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class SearchUtility {
+
+    public static Product linearSearch(Product[] products, int targetId) {
+        for (Product product : products) {
+            if (product.productId == targetId) {
+                return product;
+            }
+        }
+        return null;
+    }
+
+    public static Product binarySearch(Product[] products, int targetId) {
+        Arrays.sort(products, Comparator.comparingInt(p -> p.productId));
+
+        int left = 0, right = products.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (products[mid].productId == targetId) {
+                return products[mid];
+            } else if (products[mid].productId < targetId) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return null;
+    }
+}
